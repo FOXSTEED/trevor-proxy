064415d6507170d71ac9a32ba9d0ebbfff330ae3
Zachary <zinouye@gmail.com>
2018-03-10 11:07:32 -0800

    updated proxy to work with relative paths on module

--- proxy/server.js
+++ proxy/server.js
@@ -2,14 +2,26 @@ const express = require('express')
 // const morgan = require('morgan');
 const cors = require('cors');
 const path = require('path');
+const axios = require('axios');
 const app = express();
-const port = process.env.PORT || 1337;
+const port = process.env.PORT || 3000;
 
 
 // app.use(morgan('dev'));
 // app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/listings/:listing_id', express.static(path.join(__dirname, 'public')));
+app.use(':listing_id', express.static(path.join(__dirname, 'public')));
+app.use('/recommendations', (req, res) => {
+  axios.get(`http://localhost:3005${req.originalUrl}`)
+    .then(res => res.data)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send();
+    });
+});
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`)
